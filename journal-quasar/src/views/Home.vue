74a6bd335219a36c1768f5a5f3e89f54e40daafb
Lucas M <lucassanntos01@gmail.com>
2022-03-07 16:53:04 -0300

    init

---
+++ journal-quasar/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <h1>Hola Mundo</h1>
+
+    <button @click="goToDaybook" class="btn btn-primary">Primary</button>
+    <button @click="goToDaybook" class="btn btn-secondary">Secondary</button>
+    <button @click="goToDaybook" class="btn btn-success">Success</button>
+
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const goToDaybook = () => {
+      this.$router.push({ name: 'no-entry' })
+    }
+
+    return { goToDaybook }
+  }
+})
+</script>
